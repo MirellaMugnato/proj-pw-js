423773404df8191707285eda9b4c9ca98b689aac
Mirella <mirella.mugnato09@gmail.com>
2024-09-16 18:16:32 -0300

    aulas 03/4

--- aulas/aula04/formulario.js
+++ aulas/aula04/formulario.js
@@ -10,30 +10,31 @@ function adicionarLinha () {
     // se for preenchido vazio
     if(nome === '' || idade === '' || admissao === '') {
         alert("Preencha todos os campos")
+    } else { 
+        // cria uma linha da tabela se não existir
+        const tabela = document.getElementById("tabelaDados").getElementsByTagName ('tbody') [0];
+    
+        // inserindo uma nova linha
+        const novaLinha = tabela.insertRow();
+        
+        // inserindo os valores da linha
+        const celulaId = novaLinha.insertCell(0);
+        const celNome = novaLinha.insertCell(1);
+        const celIdade = novaLinha.insertCell(2);
+        const celAdmissao = novaLinha.insertCell(3);
+        const celDemissao = novaLinha.insertCell(4);
+    
+        // inserindo os valores das celulas
+        celulaId.innerHTML = proximoId;
+        celNome.innerHTML = nome;
+        celIdade.innerHTML = idade;
+        celAdmissao.innerHTML = admissao;
+        celDemissao.innerHTML = demissao;
+    
+        proximoId++
+    
+        // limpar formulario
+        document.getElementById("linhaForm").reset();}
     }
 
-    // cria uma linha da tabela se não existir
-    const tabela = document.getElementById("tabelaDados").getElementsByTagName ('tbody') [0];
-
-    // inserindo uma nova linha
-    const novaLinha = tabela.insertRow();
-    
-    // inserindo os valores da linha
-    const celulaId = novaLinha.insertCell(0);
-    const celNome = novaLinha.insertCell(1);
-    const celIdade = novaLinha.insertCell(2);
-    const celAdmissao = novaLinha.insertCell(3);
-    const celDemissao = novaLinha.insertCell(4);
-
-    // inserindo os valores das celulas
-    celulaId.innerHTML = proximoId;
-    celNome.innerHTML = nome;
-    celIdade.innerHTML = idade;
-    celAdmissao.innerHTML = admissao;
-    celDemissao.innerHTML = demissao;
-
-    proximoId++
-
-    // limpar formulario
-    document.getElementById("linhaForm").reset();
-}
+   
